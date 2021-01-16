bb9deda1389f6af418734d380e30aedaa3b6543b
Todd Shevlin <todd.shevlin@gmail.com>
2021-01-16 13:18:56 -0500

    some updates

--- ml.py
+++ ml.py
@@ -1,12 +1,29 @@
 class LSTM:
+    """
+    Initialize, then run 'predict'
+    Initializing will set the following default values:
+      dataframe (passed in)
+      y_col =0
+      num_units = 10
+      dropout = 0.2
+      epochs = 15
+      batch_size = 2
+      window_size = 10
+      train_size = 0.7
+      
+    predict requires feature_col (int) and
+    target_col (int), returns dataframe
+    
+    """
+    
+    import numpy as np
+    import pandas as pd
+    import hvplot.pandas
+    from sklearn.preprocessing import MinMaxScaler
+    from tensorflow.keras.models import Sequential
+    from tensorflow.keras.layers import LSTM, Dense, Dropout
+    
     def __init__(self, df = None):
-        #Move these to the top?
-        import numpy as np
-        import pandas as pd
-        import hvplot.pandas
-        from sklearn.preprocessing import MinMaxScaler
-        from tensorflow.keras.models import Sequential
-        from tensorflow.keras.layers import LSTM, Dense, Dropout
         #set default values
         
         self.df = df
@@ -16,6 +33,7 @@ class LSTM:
         self.epochs = 15
         self.batch_size = 2
         self.window_size = 10
+        self.train_size = 0.7
         
     def window_data(self,feature_col_number, target_col_number):
         X = []
@@ -38,23 +56,18 @@ class LSTM:
         self.batch_size = val
     def set_window_size(self, val):
         self.window_size = val
+    def set_train_size(self,val):
+        self.train_size = val
         
-    def predict(self,feature_col=None, target_col=None):
-        """
-        Dataframe must be prepared prior to passing in
-        will return prediction dataframe
-        """
-        X, y = window_data(self.df, window_size, feature_col, target_col)
-        
-        #Make separate method?
-        split = int(0.7 * len(X))
+    def split_me(self, X, y):
+        split = int(self.train_size * len(X))
         X_train = X[: split -1]
         X_test = X[split:]
         y_train = y[: split -1]
         y_test = y[split:]
-        #----------------------
+        return X_train, x_test, y_train, y_test
         
-        #Make separate method?
+    def scale_me(self, X_train,_X_test, y_train, y_test):
         scaler = MinMaxScaler().fit(X)
         X_train = scaler.transform(X_train)
         X_test = scaler.transform(X_test)
@@ -64,8 +77,19 @@ class LSTM:
 
         X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
         X_test = X_test.reshape((X_test.shape[0], X_test.shape[1],1))
-        #-------------------------------------------------------------------
+        return X_train,X_test,y_train,y_test
         
+    def predict(self,feature_col=None, target_col=None):
+        """
+        Dataframe must be prepared prior to passing in
+        will return prediction dataframe
+        """
+        X, y = window_data(self.df, self.window_size, feature_col, target_col)
+        
+        X_train, X_test, y_train, y_test = split_me(X,y,self.train_size)        
+        X_train, X_test, y_train, y_test = scale_me(X_train_X_test,y_train,y_test)
+        
+        #Might be tricky to separate this out right now due to coding for layers
         model = Sequential()
         number_units = self.num_units
         dropout_fraction = self.dropout
@@ -108,7 +132,28 @@ class LSTM:
 
         return stocks
     
-    
+
         #expand on this later
         def plot_me(self, df):
             return df.plot()
+        
+        def hvscatter(self,df,x,y, title = "Scatter Plot"):
+            import plotly.express as px
+            pn.extension('plotly')
+            import hvplot.pandas
+            return df.hvplot.scatter(
+                x = x,
+                y = y,
+                title = title
+            )
+        
+        def pxscatter(self,df,x,y,title = "Scatter Plot"):
+            import plotly.express as px
+            pn.extension('plotly')
+            import hvplot.pandas
+            return px.scatter(
+                df,
+                x = x,
+                y = y,
+                title = title
+            )
