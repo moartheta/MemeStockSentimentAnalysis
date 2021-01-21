16d4c98641b9430e257c757a74955a7069d537fa
Todd Shevlin <todd.shevlin@gmail.com>
2021-01-21 17:27:22 -0500

    blob optimized, rewrote as classes

--- nlp.py
+++ nlp.py
@@ -3,156 +3,121 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import pandas as pd
 import numpy as np
 from textblob import TextBlob
+from textblob import Blobber
 from textblob.sentiments import NaiveBayesAnalyzer
 
-def initialize():
-    nltk.download('vader_lexicon')
+
+class NLT:
+    def __init__(self):
+        pass
+    
+    def update_vader(self):
+        nltk.download('vader_lexicon')
 
     
-def make_sentiment_df(df, src = "Reddit"):
-    analyzer = SentimentIntensityAnalyzer()
-    sentiments = []
+    def make_sentiment_df(self,df, src = "Reddit"):
+        analyzer = SentimentIntensityAnalyzer()
+        sentiments = []
     
-    #adjust later for other API's
-    if src.lower() == "reddit":
-        for index,row in df.iterrows():
-            try:
-                title = row["Title"]
-                upvote = row["Upvote Ratio"]
-                text = row["Text"]
-                sentiment = analyzer.polarity_scores(title)
-                title_compound = sentiment["compound"]
-                title_pos = sentiment["pos"]
-                title_neu = sentiment["neu"]
-                title_neg = sentiment["neg"]
-                sentiment = analyzer.polarity_scores(text)
-                #To account for blank text values
-                if len(text) > 0:
-                    text_compound = sentiment["compound"]
-                    text_pos = sentiment["pos"]
-                    text_neu = sentiment["neu"]
-                    text_neg = sentiment["neg"]
-                else:
-                    text_compound = np.nan
-                    text_pos = np.nan
-                    text_neu = np.nan
-                    text_neg = np.nan
+        #adjust later for other API's
+        if src.lower() == "reddit":
+            #for index,row in df.iterrows():
+            #This one is faster if it works
+            for index, row in df.itertuples():
+                try:
+                    title = row["Title"]
+                    upvote = row["Upvote Ratio"]
+                    text = row["Text"]
+                    sentiment = analyzer.polarity_scores(title)
+                    title_compound = sentiment["compound"]
+                    title_pos = sentiment["pos"]
+                    title_neu = sentiment["neu"]
+                    title_neg = sentiment["neg"]
+                    sentiment = analyzer.polarity_scores(text)
+                    #To account for blank text values
+                     #commenting out unless we use text
+#                   if len(text) > 0:
+#                     text_compound = sentiment["compound"]
+#                     text_pos = sentiment["pos"]
+#                     text_neu = sentiment["neu"]
+#                     text_neg = sentiment["neg"]
+#                   else:
+#                     text_compound = np.nan
+#                     text_pos = np.nan
+#                     text_neu = np.nan
+#                     text_neg = np.nan
 
-                sentiments.append({
-                    "Title": title,
-                    "Upvote_Ratio": upvote,
-                    "Title_Compound": title_compound,
-                    "Title_Positive": title_pos,
-                    "Title_Negative": title_neg,
-                    "Title_Neutral" : title_neu,
-                    "Text_Compound" : text_compound,
-                    "Text_Positive" : text_pos,
-                    "Text_Negative" : text_neg,
-                    "Text_Neutral" : text_neu
-                })
-            except AttributeError:
-                pass
+                    sentiments.append({
+                        "Title": title,
+                        "Upvote_Ratio": upvote,
+                        "Reddit_Title_Compound": title_compound,
+                        "Reddit_Title_Positive": title_pos,
+                        "Reddit_Title_Negative": title_neg,
+                        "Reddit_Title_Neutral" : title_neu,
+#                     "Text_Compound" : text_compound,
+#                     "Text_Positive" : text_pos,
+#                     "Text_Negative" : text_neg,
+#                     "Text_Neutral" : text_neu
+                    })
+                except AttributeError:
+                    pass
         
-    df = pd.DataFrame(sentiments)
-    #reorder columns if needed
-    #cols = ["date", "text", ...]
-    #df = df[cols]
-    return df 
+        df = pd.DataFrame(sentiments)
+            #reorder columns if needed
+            #cols = ["date", "text", ...]
+            #df = df[cols]
+        return df 
 
-def show_stats(df):
-    return df.describe()
+    def show_stats(self,df):
+        return df.describe()
     
-def sentiment_scaler(score):    
-    result = 0
-    if score >= 0.05:
-        result = 1
-    elif score < 0.05:
-        result = -1
+    def sentiment_scaler(self,score):    
+        result = 0
+        if score >= 0.05:
+            result = 1
+        elif score < 0.05:
+            result = -1
         
-    return result
+        return result
 
+class Blobby:
+    def __init__(self):
+        pass
+    
+    def get_blob(self,text):
+        blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
+        return blob.sentiment
 
-def get_blob(text):
-    blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
-    return blob.sentiment
+    def get_blob_all(self,text):
+        blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
+        return blob.sentiment.classification, blob.sentiment.p_pos, blob.sentiment.p_neg
 
-def get_blob_class(text):
-    blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
-    return blob.sentiment.classification
+    def get_blob_class(self,text):
+        blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
+        return blob.sentiment.classification
     
-def get_blob_pos(text):
-    blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
-    return float(blob.sentiment.p_pos)
+    def get_blob_pos(self,text):
+        blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
+        return float(blob.sentiment.p_pos)
     
-def get_blob_neg(text):
-    blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
-    return float(blob.sentiment.p_neg)
+    def get_blob_neg(self,text):
+        blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
+        return float(blob.sentiment.p_neg)
 
-def add_blob(df):
-   # blobbies = []
-    
-    df = df.copy()
-    
-    #classif = [get_blob_class(x) for x in df["Title"]]
-    #pos = [get_blob_pos(x) for x in df["Title"]]
-    #neg = [get_blob_neg(x) for x in df["Title"]]
-    
-    #print(classif)
-    #print(pos)
-    #print(neg)
-    
-    #----This Works, but slooowww
-    df["Blob Class"] = df["Title"].apply(get_blob_class)
-    df["Blob Pos"] = df["Title"].apply(get_blob_pos)
-    df["Blob Neg"] = df["Title"].apply(get_blob_neg)
-    #----------------------------------
- 
-    return df
+    def add_blob(self,df):
+        tb = Blobber(analyzer = NaiveBayesAnalyzer())
+        df = df.copy()
 
-   # for index,row in df.iterrows():
-   #     blob = TextBlob(row["Title"], analyzer=NaiveBayesAnalyzer())
-    #    classif = blob.sentiment.classification
-  #      pos = blob.sentiment.p_pos
-   #     neg = blob.sentiment.p_neg
-                
- #       df.append({
-  #          "Blob Class" : classif,
-   #         "Blob Pos" : pos,
-    #        "Blob Neg" : neg
-     #   })
-      #  print(classif)
-        
-#     df_blob = pd.DataFrame(blobbies)
-#     return df_blob
-   # return df
+        classif = []
+        pos = []
+        neg = []
     
+        for idx, row in enumerate(df.itertuples(index = False)):
+            classif.append(tb(df["Title"][idx]).sentiment.classification)
+            pos.append(tb(df["Title"][idx]).sentiment.p_pos)
+            neg.append(tb(df["Title"][idx]).sentiment.p_neg)
     
-    
-#     df["Blob_Class"] = ""
-#     df["Blob_Pos"] = 0.0
-#     df["Blob_Neg"] = 0.0
-    
-#     for item in df["Title"]:
-#         print(item)
-#         s = get_blob(item)
-#         #df["Blob_Class"].append(get_blob_class(item))
-#         #df["Blob_Pos"].append(get_blob_pos(item))
-#         #df["Blob_Neg"].append(get_blob_neg(item))
-#         df["Blob_Class"].append(s.classification)
-#         df["Blob_Pos"].append(s.p_pos)
-#         df["Blob_Neg"].append(s.p_neg)
-     
-#     return df
-# #     for row, index in df.iterrows():
-# #         classif.append(list(get_blob_class(row[0])))
-# #         pos.append(get_blob_pos(row["Title"]))
-# #         neg.append(get_blob_neg(row["Title"]))
-        
-# #     df["Blob_Class"] = classif
-# #     df["Blob_Pos"] = pos
-# #     df["Blob_Neg"] = neg
-    
-# #    return df
-# #        df["Blob_Class"] = map(lambda x: get_blob_class(x), df["Title"])
-#  #       df["Blob_Pos"] = list(map(get_blob_pos,df["Title"]))
-#   #      df["Blob_Neg"] = [x: for item in list item == get_blob_neg(x)]
+        df["Blob Class"] = classif
+        df["Blob Pos"]= pos
+        df["Blob Neg"] = neg
+        return df
